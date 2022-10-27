7086e0240c8a8cf9e4eaf588d82d2c0c41ff1773
Juan David Cruz Garcia <angrybirdsgallo@gmail.com>
2022-10-27 15:42:07 -0500

    Data to database

--- src/main/java/com/icesi/edu/users/UsersApplication.java
+++ src/main/java/com/icesi/edu/users/UsersApplication.java
@@ -2,9 +2,11 @@ package com.icesi.edu.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@Import({com.edu.icesi.restzooregisters.integration.config.InitialDataConfig.class})
 @EnableJpaRepositories
 public class UsersApplication {
 
