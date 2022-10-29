1aad1d213899c73d46c908df6c2c592bca6b63f6
Kennet Santiago Sánchez Roldán <1006229463@u.icesi.edu.co>
2022-10-29 11:40:18 -0500

    Exception handler updated to manage annotations
    * Now the exception handler can manage annotations exceptions
    
    * The error codes has been updated

--- src/main/java/com/icesi/edu/users/constant/UserErrorCode.java
+++ src/main/java/com/icesi/edu/users/constant/UserErrorCode.java
@@ -3,12 +3,19 @@ package com.icesi.edu.users.constant;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.persistence.AssociationOverride;
+
 @AllArgsConstructor
 @Getter
 public enum UserErrorCode {
 
+    CODE_00("Restart your pc, we couldn't track the error."),
     CODE_01("The password doesn't fulfill the character diversity"),
     CODE_02("The email doesn't belong to the expected domain."),
+    CODE_03("The phone doesn't follows the expected pattern."),
+    CODE_04("The name can take only letters."),
+    CODE_05("The last name can take only letters."),
+
     CODE_401("Unauthorized. Authentication needed to do this request.");
 
     private final String message;
