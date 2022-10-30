903065b0ee89c2ff458beeb01a459da605ab3527
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2022-10-29 21:01:13 -0500

    Validations created

--- src/main/java/com/icesi/edu/users/validation/CustomAnnotations.java
+++ src/main/java/com/icesi/edu/users/validation/CustomAnnotations.java
@@ -35,6 +35,34 @@ public interface CustomAnnotations {
         Class<? extends Payload>[] payload() default {};
 
     }
+
+    @Documented
+    @Constraint(validatedBy = PasswordValidator.class)
+    @Target({ ElementType.METHOD, ElementType.FIELD })
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface PasswordValidation {
+
+        String message() default "Password is invalid";
+
+        Class<?>[] groups() default {};
+
+        Class<? extends Payload>[] payload() default {};
+
+    }
+
+    @Documented
+    @Constraint(validatedBy = LoggedUserValidator.class)
+    @Target({ ElementType.METHOD, ElementType.PARAMETER })
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface LoggedUserValidation {
+
+        String message() default "User is not logged";
+
+        Class<?>[] groups() default {};
+
+        Class<? extends Payload>[] payload() default {};
+
+    }
 }
 
 
