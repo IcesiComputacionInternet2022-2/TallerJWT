1b9f161fbbf1b4b21b999dbc6370d1771caaf825
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2022-10-29 21:02:18 -0500

    nothing happened

--- src/main/java/com/icesi/edu/users/dto/LoginDTO.java
+++ src/main/java/com/icesi/edu/users/dto/LoginDTO.java
@@ -1,6 +1,9 @@
 package com.icesi.edu.users.dto;
 
+import com.icesi.edu.users.error.exception.LoginException;
 import lombok.Data;
+
+import javax.validation.constraints.Pattern;
 
 @Data
 public class LoginDTO {
