d73591daf7c1771d620853ad81f9fd59ff729a88
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2022-10-29 20:58:32 -0500

    fromCreateDTO method created

--- src/main/java/com/icesi/edu/users/mapper/UserMapper.java
+++ src/main/java/com/icesi/edu/users/mapper/UserMapper.java
@@ -2,25 +2,33 @@ package com.icesi.edu.users.mapper;
 
 import com.icesi.edu.users.dto.UserCreateDTO;
 import com.icesi.edu.users.dto.UserDTO;
+import com.icesi.edu.users.dto.UserWithParentsDTO;
 import com.icesi.edu.users.model.User;
+import org.mapstruct.MapMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.UUID;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
-    User fromDTO(UserDTO userDTO);
+     User fromDTO(UserDTO userDTO);
+     UserDTO fromUser(User user);
 
-    UserDTO fromUser(User user);
+     User fromCreateDTO(UserCreateDTO userCreateDTO);
 
-    User fromDTO(UserCreateDTO userCreateDTO);
+     /*@Mapping(source = "child.id", target = "id")
+     @Mapping(source = "child.name", target= "name")
+     @Mapping(source = "parent", target = "padre")
+     @Mapping(source = "mother", target = "madre")
+     UserWithParentsDTO fromUsers(User child, User parent, User mother);
 
-    default String fromUUID(UUID uuid) {
-        return uuid.toString();
-    }
+     default String fromUUID(UUID uuid){
+          return uuid.toString();
+     }
 
-    default UUID fromUUID(String uuid) {
-        return UUID.fromString(uuid);
-    }
+     default UUID fromUUID(String uuid){
+          return UUID.fromString(uuid);
+     }*/
 }
